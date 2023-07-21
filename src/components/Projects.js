320a5b77328c77d11c61145415d7e3c4ee487d2a
nitesh32 <nit8339@gmail.com>
2023-07-21 11:09:00 +0530

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from "react";
 import * as Icon from "react-bootstrap-icons";
-import Proj from './projects/Proj';
-
+import Proj from "./projects/Proj";
 
 export default function Projects() {
-  
   return (
     <div className="div-all" id="projects">
       <div className="naming_of_div">
@@ -12,21 +10,79 @@ export default function Projects() {
         &nbsp;My&nbsp;<strong>Projects</strong>
       </div>
       <div id="main_projects">
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="1"/>
-       <Proj name="Image Carousel." desc="it is a image carousel of 5 images in 3d prespective ." photo="/assets/images/gallery_1.png" id="2" link="https://nitesh32.github.io/gallery/" git="https://github.com/nitesh32/gallery"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="3"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="4"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="5"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="6"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="7"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="8"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="9"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="10"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="11"/>
-       <Proj name="benemart" photo="/assets/images/img_2.jpeg" id="12"/>
-     
-      
+        <Proj
+          name="Benemart"
+          photo="/assets/images/benemart_1.png"
+          desc="It Is a Shopping Website Which Has Authentication, Database and Express Server ."
+          link="https://github.com/nitesh32/benemart"
+          git="https://github.com/nitesh32/benemart"
+          id="1"
+        />
+        <Proj
+          name="Image Carousel."
+          desc="It Is a Image Carousel Of 5 Images In 3d Prespective ."
+          photo="/assets/images/gallery_1.png"
+          id="2"
+          link="https://nitesh32.github.io/gallery/"
+          git="https://github.com/nitesh32/gallery"
+        />
+        <Proj
+          name="Ping Pong Game"
+          desc="It Is a Web Game of Two Rodes And One Ball ."
+          photo="/assets/images/pingpong.png"
+          id="3"
+          link="https://nitesh32.github.io/pingpong_js/"
+          git="https://github.com/nitesh32/pingpong_js"
+        />
+        <Proj
+          name="Calculator"
+          desc="Calculator Using HTML CSS JS Having Dark And Light Modes ."
+          photo="/assets/images/calculator.png"
+          id="4"
+          link="https://nitesh32.github.io/calc/"
+          git="https://github.com/nitesh32/calc"
+        />
+        <Proj
+          name="To-Do Using Api"
+          desc="TODO List Using Api And Alert Message On Every Move ."
+          photo="/assets/images/todo_api.png"
+          id="5"
+          link="https://nitesh32.github.io/todo/"
+          git="https://github.com/nitesh32/todo"
+        />
+        <Proj
+          name="To-Do Using Database"
+          desc="TODO List Using Database Server and Task Addition And Deletion ."
+          photo="/assets/images/todo_data.png"
+          id="6"
+          link="https://github.com/nitesh32/todo_mern"
+          git="https://github.com/nitesh32/todo_mern"
+        />
+        <Proj
+          name="Tilt Image Effect"
+          desc="Tilt Image Effect By Using Javascript ."
+          photo="/assets/images/parallax.png"
+          id="7"
+          link="https://nitesh32.github.io/parallax_effect_img/"
+          git="https://github.com/nitesh32/parallax_effect_img"
+        />
+        <Proj
+          name="Catch Me Game"
+          desc="Game In Which Section Is Changing Its Position On Hover Every Time ."
+          photo="/assets/images/catch.png"
+          id="8"
+          link="https://nitesh32.github.io/catch_me/"
+          git="https://github.com/nitesh32/catch_me"
+        />
+        <Proj
+          name="Music Player Page"
+          desc="Static Website Of A Music Player Using HTML CSS JS ."
+          photo="/assets/images/music.png"
+          id="9"
+          link="https://nitesh32.github.io/music_player//"
+          git="https://github.com/nitesh32/music_player"
+        />
       </div>
     </div>
-  )
+  );
 }
--- src/components/projects/Proj.js
+++ src/components/projects/Proj.js
@@ -5,22 +5,28 @@ import * as Icon from "react-bootstrap-icons";
 export default function Proj(props) {
     const funchere = (val)=>{
         const ele = document.getElementById(val);
-        ele.style.display="block";
+        ele.style.transition=0.5+"s";
+        ele.style.top=15+"%";
+        console.log(ele);
+    
     }
     const funcheree = (val)=>{
         const ele = document.getElementById(val);
-        ele.style.display="none";
+        ele.style.top=80+"%";
+        console.log(ele);
     }
   return (
     <Tilt>
-      <div className="card" onMouseEnter={() => {funchere(props.id);}} onMouseLeave={() => {funcheree(props.id);}}  >
+      <div className="card" onMouseEnter={() => {funchere(props.id);}} onMouseLeave={() => {funcheree(props.id);}}  data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="1000">
         <img
           src={process.env.PUBLIC_URL + props.photo}
           alt="project"
         ></img>
-        <div className="about_project">
+        <div className="about_project" id={props.id}>
           <span>{props.name}</span>
-          <div className="about_project_desc animate fade-down" id={props.id}>
+          <div className="about_project_desc animate fade-down" >
             <span>
               {props.desc}
             </span>
